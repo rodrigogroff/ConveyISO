08063d58a76ad35e8555feeb0846c3e1d862c4fb
rodrigo groff <rodrigo.groff@gmail.com>
2018-02-16 00:07:05 -0200

    fix

---
+++ ServerISO/Program.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections;
+using System.Threading;
+
+#region - ClientConnectionPool - 
+
+public class ClientConnectionPool
+{
+    Queue SyncdQ = Queue.Synchronized(new Queue());
+
+    public void Enqueue(ClientHandler client)
+    {
+        SyncdQ.Enqueue(client);
+    }
+
+    public ClientHandler Dequeue()
+    {
+        return (ClientHandler)(SyncdQ.Dequeue());
+    }
+
+    public int Count
+    {
+        get { return SyncdQ.Count; }
+    }
+
+    public object SyncRoot
+    {
+        get { return SyncdQ.SyncRoot; }
+    }
+}
+
+#endregion
+
+#region - ClientService - 
+
+public class ClientService
+{
+    const int NUM_OF_THREAD = 10;
+
+    ClientConnectionPool ConnectionPool;
+    Thread[] ThreadTask = new Thread[NUM_OF_THREAD];
+
+    bool ContinueProcess = false;
+
+    public ClientService(ClientConnectionPool ConnectionPool)
+    {
+        this.ConnectionPool = ConnectionPool;
+    }
+
+    public void Start()
+    {
+        ContinueProcess = true;
+
+        // Start threads to handle Client Task
+        for (int i = 0; i < ThreadTask.Length; i++)
+        {
+            ThreadTask[i] = new Thread(new ThreadStart(this.Process));
+            ThreadTask[i].Start();
+        }
+    }
+
+    void Process()
+    {
+        while (ContinueProcess)
+        {
+            ClientHandler client = null;
+
+            lock (ConnectionPool.SyncRoot)
+            {
+                if (ConnectionPool.Count > 0)
+                    client = ConnectionPool.Dequeue();
+            }
+
+            if (client != null)
+            {
+                client.Process();
+
+                if (client.Alive)
+                    ConnectionPool.Enqueue(client);
+            }
+
+            Thread.Sleep(100);
+        }
+    }
+
+    public void Stop()
+    {
+        ContinueProcess = false;
+
+        for (int i = 0; i < ThreadTask.Length; i++)
+        {
+            if (ThreadTask[i] != null && ThreadTask[i].IsAlive)
+                ThreadTask[i].Join();
+        }
+
+        while (ConnectionPool.Count > 0)
+        {
+            ClientHandler client = ConnectionPool.Dequeue();
+            client.Close();
+            Console.WriteLine("Client connection is closed!");
+        }
+    }
+}
+
+#endregion
+
+public class SynchronousSocketListener
+{
+    public static int Main(String[] args)
+    {
+        Console.WriteLine("\nServer port...");
+        var customPort = Console.ReadLine();
+
+        if (customPort.Length > 0)
+            portNum = Convert.ToInt32(customPort);
+
+        Console.WriteLine("\nPort is: " + portNum);
+
+        StartListening();
+
+        Console.WriteLine("\nHit enter to continue...");
+        Console.Read();
+
+        return 0;
+    }
+
+    public static int portNum = 2700;
+
+    public static void StartListening()
+    {
+        var ConnectionPool = new ClientConnectionPool();
+        var ClientTask = new ClientService(ConnectionPool);
+
+        ClientTask.Start();
+
+        var listener = new TcpListener(portNum);
+
+        try
+        {
+            listener.Start();
+
+            int ClientNbr = 0;
+
+            // Start listening for connections.
+            Console.WriteLine("Waiting for a connection...");
+
+            while (true)
+            {
+                TcpClient handler = listener.AcceptTcpClient();
+
+                if (handler != null)
+                {
+                    Console.WriteLine("Client#{0} accepted!", ++ClientNbr);
+                    ConnectionPool.Enqueue(new ClientHandler(handler));
+                }
+
+                Thread.Sleep(100);
+            }
+
+            //listener.Stop();
+            //ClientTask.Stop();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+}
+
+public class ClientHandler
+{
+    TcpClient ClientSocket;
+    NetworkStream networkStream;
+
+    bool ContinueProcess = false;
+    byte[] bytes;
+
+    StringBuilder sb = new StringBuilder();
+    string data = null;
+
+    public ClientHandler(TcpClient ClientSocket)
+    {
+        ClientSocket.ReceiveTimeout = 1000; // 100 miliseconds
+        this.ClientSocket = ClientSocket;
+        networkStream = ClientSocket.GetStream();
+        bytes = new byte[ClientSocket.ReceiveBufferSize];
+        ContinueProcess = true;
+    }
+
+    public void Process()
+    {
+        try
+        {
+            int BytesRead = networkStream.Read(bytes, 0, (int)bytes.Length);
+
+            if (BytesRead > 0)
+                // There might be more data, so store the data received so far.
+                sb.Append(Encoding.ASCII.GetString(bytes, 0, BytesRead));
+            else
+                // All the data has arrived; put it in response.
+                ProcessDataReceived();
+        }
+        catch (IOException)
+        {
+            // All the data has arrived; put it in response.
+            ProcessDataReceived();
+        }
+        catch (SocketException)
+        {
+            networkStream.Close();
+            ClientSocket.Close();
+            ContinueProcess = false;
+            Console.WriteLine("Conection is broken!");
+        }
+    }
+
+    private void ProcessDataReceived()
+    {
+        if (sb.Length > 0)
+        {
+            bool bQuit = (String.Compare(sb.ToString(), "quit", true) == 0);
+
+            data = sb.ToString();
+
+            sb.Length = 0; // Clear buffer
+
+            Console.WriteLine("Text received from client:");
+            Console.WriteLine(data);
+
+            StringBuilder response = new StringBuilder();
+            response.Append("Received at ");
+            response.Append(DateTime.Now.ToString());
+            response.Append("\r\n");
+            response.Append(data);
+
+            // Echo the data back to the client.
+            byte[] sendBytes = Encoding.ASCII.GetBytes(response.ToString());
+            networkStream.Write(sendBytes, 0, sendBytes.Length);
+
+            // Client stop processing  
+            if (bQuit)
+            {
+                networkStream.Close();
+                ClientSocket.Close();
+                ContinueProcess = false;
+            }
+        }
+    }
+
+    public void Close()
+    {
+        networkStream.Close();
+        ClientSocket.Close();
+    }
+
+    public bool Alive
+    {
+        get
+        {
+            return ContinueProcess;
+        }
+    }
+}
