906fc5e28c68990a12f20ec64d2c4760fd9344a9
rodrigo groff <rodrigo.groff@gmail.com>
2018-04-05 22:59:11 -0300

    fix

---
+++ TesteREDE/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteREDE
+{
+    class SimulaISO_Program
+    {
+        static void Main(string[] args)
+        {
+            int portNum = 2700;
+
+            var tcpClient = new TcpClient();
+
+            try
+            {
+                Console.WriteLine("IP? [1] localhost [2] 138.118.164.191 ");
+
+                string myIp = Console.ReadLine();
+
+                if (myIp == "1" || myIp == "")
+                    myIp = "localhost";
+                else if (myIp == "2")
+                    myIp = "138.118.164.191";
+
+                Console.WriteLine(">> " + myIp);
+
+                Console.WriteLine("port? [2700]");
+
+                string myPort = Console.ReadLine();
+
+                tcpClient.Connect(myIp, portNum);
+                NetworkStream networkStream = tcpClient.GetStream();
+
+                if (networkStream.CanWrite)
+                {
+                    var DataToSend = "PING";
+
+                    {
+                        Byte[] sendBytes = Encoding.ASCII.GetBytes(DataToSend);
+                        networkStream.Write(sendBytes, 0, sendBytes.Length);
+                        networkStream.Flush();
+                    }
+
+                    Console.WriteLine("\nEnviado " + DataToSend);
+
+                    {
+                        // Reads the NetworkStream into a byte buffer.
+                        byte[] bytes = new byte[tcpClient.ReceiveBufferSize];
+                        int BytesRead = networkStream.Read(bytes, 0, (int)tcpClient.ReceiveBufferSize);
+
+                        // Returns the data received from the host to the console.
+                        string returndata = Encoding.UTF7.GetString(bytes, 0, BytesRead);
+
+                        Console.WriteLine("\nRecebido " + returndata);
+
+                        if (returndata == "PONG")
+                            Console.WriteLine("\n ======== TESTE OK ============ ");
+                        else
+                            Console.WriteLine("\n ############ !!! TESTE NAO OK !!! #################### ");
+                    }
+                }
+
+                networkStream.Close();
+                tcpClient.Close();
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Sever not available!");
+            }
+            catch (System.IO.IOException)
+            {
+                Console.WriteLine("Sever not available!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
